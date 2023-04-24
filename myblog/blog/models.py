1157760d25bd62e93133761a0771a8f7ef76cd0b
Maks <maksimuspiter@gmail.com>
2023-04-24 13:22:52 +0300

    create blog models

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+from taggit.managers import TaggableManager
 
-# Create your models here.
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
+class Post(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "DR", "Draft"
+        PUBLISHED = "PB", "Published"
+
+    title = models.CharField(max_length=250, verbose_name="Заглавие")
+    slug = models.SlugField(max_length=250, unique_for_date="publish")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_posts", verbose_name="Автор"
+    )
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now, verbose_name="Дата публикации")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Последнее изменение")
+    status = models.CharField(
+        max_length=2,
+        choices=Status.choices,
+        default=Status.DRAFT,
+        verbose_name="статус",
+    )
+    # manager
+    objects = models.Manager()
+    published = PublishedManager()
+    # ManyToMany taggit
+    tags = TaggableManager()
+
+    class Meta:
+        ordering = ["-publish"]
+        indexes = [
+            models.Index(fields=["-publish"]),
+        ]
+        verbose_name = "Пост"
+        verbose_name_plural = "Посты"
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        pass
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments", verbose_name="Пост"
+    )
+    name = models.CharField(max_length=80, verbose_name="Название")
+    email = models.EmailField()
+    body = models.TextField(verbose_name="Контент")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Последнее изменение")
+    active = models.BooleanField(default=True, verbose_name="Активный")
+
+    class Meta:
+        ordering = ["created"]
+        indexes = [
+            models.Index(fields=["created"]),
+        ]
+        verbose_name = "Комментарий"
+        verbose_name_plural = "Комментарии"
+
+    def __str__(self):
+        return f"Комментарий {self.name} на пост {self.post}"

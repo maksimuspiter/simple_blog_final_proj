d5364a81369428a73a10def2f237498b9c35fba0
Maks <maksimuspiter@gmail.com>
2023-04-24 15:12:21 +0300

    add paginator

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -5,4 +5,5 @@ app_name = "blog"
 
 urlpatterns = [
     path("", views.all_posts, name="all-posts"),
+    path("posts", views.PostListView.as_view(), name="all-posts"),
 ]
--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post, Comment
 
 
 def all_posts(request):
     posts = Post.objects.all()
     return render(request, "blog/post/list.html", {"posts": posts})
+
+
+class PostListView(ListView):
+    paginate_by = 1
+    model = Post
+    template_name = "blog/post/list.html"
+    context_object_name = 'posts'

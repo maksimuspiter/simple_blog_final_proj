f92ada3f8bd78e2a5b826fd5169582c99785ccac
Maks <maksimuspiter@gmail.com>
2023-04-25 16:43:30 +0300

    add logout

--- myblog/portfolio/urls.py
+++ myblog/portfolio/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
         name="portfolio-create",
     ),
     path("create2/", views.create_portfolio, name="portfolio-create2"),
-    path('registration/', views.registration, name='registration'),
-    path('login/', views.user_login, name='login')
+    path("registration/", views.registration, name="registration"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
 ]

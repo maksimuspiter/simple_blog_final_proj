a241f81042a7a21fa6785fc88ca42fe804eade10
Maks <maksimuspiter@gmail.com>
2023-05-01 12:12:19 +0300

    add debug_toolbar

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "portfolio",
     "taggit",
     "like_dislike",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
+]
+INTERNAL_IPS = [
+    "127.0.0.1",
 ]
 
 ROOT_URLCONF = "myblog.urls"

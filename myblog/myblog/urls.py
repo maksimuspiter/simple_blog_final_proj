add47583b22a6bea126b384cfb022431df30ae9a
Maks <maksimuspiter@gmail.com>
2023-05-03 16:25:27 +0300

    add debug toolbar

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("blog/", include("blog.urls", namespace="blog")),
     path("portfolio/", include("portfolio.urls", namespace="portfolio")),
     path("like_dislike/", include("like_dislike.urls", namespace="like_dislike")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
